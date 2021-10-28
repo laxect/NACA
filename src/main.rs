3637bcae669b7740d40266714210699c71eea384
gyara <me@gyara.moe>
2021-10-28 20:40:19 +0900

    gnuplot

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,35 @@
+use std::env::args;
+
+use gnuplot::{
+    AutoOption, AxesCommon, Figure,
+    PlotOption::{Caption, Color},
+};
 use naca::{NACAAirfoil, NACA4};
-use plotters::prelude::*;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let root = BitMapBackend::new("NACA.png", (1920, 1080)).into_drawing_area();
-    root.fill(&WHITE)?;
-    let mut id = String::new();
-    std::io::stdin().read_line(&mut id)?;
+    let id = args().nth(1).unwrap();
     let id = id.trim_end();
     let title = format!("NACA {}", id);
     let naca4: NACA4 = id.parse()?;
 
-    let yus: Vec<_> = (0..=10000)
-        .map(|x| x as f32 / 10000.0)
+    let (xu, yu): (Vec<f32>, Vec<f32>) = (0..=1000)
+        .map(|x| x as f32 / 1000.0)
         .map(|x| (naca4.xu(x), naca4.yu(x)))
-        .collect();
-    let yls = (0..=10000)
-        .map(|x| x as f32 / 10000.0)
-        .map(|x| (naca4.xl(x), naca4.yl(x)));
-    let high = yus
-        .iter()
-        .max_by(|(_x1, y1), (_x2, y2)| y1.abs().partial_cmp(&y2.abs()).unwrap())
-        .unwrap()
-        .1
-        + 0.1;
+        .unzip();
+    let (xl, yl): (Vec<f32>, Vec<f32>) = (0..=100)
+        .map(|x| x as f32 / 100.0)
+        .map(|x| (naca4.xl(x), naca4.yl(x)))
+        .unzip();
 
-    let mut chart = ChartBuilder::on(&root)
-        .caption(title, ("sans-serif", 50).into_font())
-        .margin(5)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_cartesian_2d(0f32..1f32, -high..high)?;
-
-    chart.configure_mesh().draw()?;
-
-    chart
-        .draw_series(LineSeries::new(yus, &RED))?
-        .label("Upper")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-    chart
-        .draw_series(LineSeries::new(yls, &RED))?
-        .label("Lower")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
-
+    let mut figure = Figure::new();
+    figure.set_title(&title);
+    figure
+        .axes2d()
+        .lines(xu, yu, &[Caption("Upper"), Color("red")])
+        .lines(xl, yl, &[Caption("Lower"), Color("red")])
+        .set_aspect_ratio(AutoOption::Fix(0.25))
+        .set_y_range(AutoOption::Fix(-0.25), AutoOption::Fix(0.25))
+        .set_x_range(AutoOption::Fix(0.0), AutoOption::Fix(1.0));
+    figure.show()?;
     Ok(())
 }
